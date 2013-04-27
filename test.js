cddff7cb520b0ba1e35767d0bb6546f4d474ce55
Jamie <jstansfield@globalpersonals.co.uk>
2013-04-27 14:23:30 +0100

    Add menu items
    Manifest permissions change, JS is run in the background straight away.

--- test.js
+++ test.js
@@ -5,16 +5,36 @@ var Clippie = {
 		  windowId: chrome.windows.WINDOW_ID_CURRENT
 		  },function(tabs){
 			var tab = tabs[0];
-			Clippie.getParameters(tab.url)
+			document.body.innerHTML = tab.url
 		});
 	},
 	getParameters: function(url) {
 		params = /#.*[?&]locale=([^&]+)(&|$)/;
 		alert(params.exec(url))
 	},
-	test:function() {
-		return "hello"
+	menu: function(){
+	    chrome.contextMenus.onClicked.addListener(clickHandler);
+		var menuNames = new Array();
+		menuNames = ["Object1","Object2"];
+		var length = menuNames.length;
+		chrome.contextMenus.create({
+		    "id" : "ClippieMain",
+			"title" : "Clippie",
+			"contexts" : ["link"]
+			});
+		
+
+		chrome.contextMenus.create({
+			"title" : "object1",
+			"id" : "sub0",
+			"parentId" : "ClippieMain",
+			"contexts": ["link"],
+			"onclick" : clickHandler
+		});
+		
+		function clickHandler(e,tab){
+		  chrome.tabs.create({"url" : "http://mypetshark.com" });
+		}
 	}
 };
-
- Clippie.getUrl();
+ Clippie.menu();
